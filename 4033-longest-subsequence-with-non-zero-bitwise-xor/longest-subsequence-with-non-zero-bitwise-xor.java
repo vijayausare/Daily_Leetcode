d974e168c729c60aa6f0800075021fddff116a37
Vijay Ausare <59534955+vijayausare@users.noreply.github.com>
2026-08-15 19:44:22 +0530

    Time: 1 ms (100.00%) | Memory: 133.4 MB (75.00%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
@@ -19,67 +19,36 @@ class Solution {
         TLE
         Analysis
         Time Complexity: O(n²)
-        Space Complexity: O(n)
+        Space Complexity: O(1)
         */ 
     }
 
-    // public int bitOptimizedSolution(int[] nums) {
-    //     int n = nums.length;
-
-    //     int xor = 0;
-    //     for (int num : nums) {
-    //         xor ^= num;
-    //     }
-
-    //     if (xor != 0) {
-    //         return n;
-    //     }
-
-    //     xor = 0;
-    //     int removeFromLeft = n;
-
-    //     for (int i = 0; i < n; i++) {
-    //         xor ^= nums[i];
-
-    //         if (xor != 0) {
-    //             removeFromLeft = i + 1;
-    //             break;
-    //         }
-    //     }
-
-    //     xor = 0;
-    //     int removeFromRight = n;
-    //     for (int i = n - 1; i >= 0; i--) {
-    //         xor ^= nums[i];
-
-    //         if (xor != 0) {
-    //             removeFromRight = n - i;
-    //             break;
-    //         }
-    //     }
-
-    //     return Math.max(n - removeFromLeft, n - removeFromRight);
-    // }
-
     public int bitOptimizedSolution(int[] nums) {
-    int xor = 0;
+        int xor = 0;
 
-    for (int num : nums) {
-        xor ^= num;
-    }
-
-    if (xor != 0) {
-        return nums.length;
-    }
-
-    for (int num : nums) {
-        if (num != 0) {
-            return nums.length - 1;
+        for (int num : nums) {
+            xor ^= num;
         }
-    }
 
-    return 0;
-}
+        if (xor != 0) {
+            return nums.length;
+        }
+
+        for (int num : nums) {
+            if (num != 0) {
+                return nums.length - 1;
+            }
+        }
+
+        return 0;
+
+        /* 
+        TLE
+        Analysis
+        Time Complexity: O(n) + O(n) = O(n)
+        Space Complexity: O(1)
+        */ 
+    }
 
 
     public int longestSubsequence(int[] nums) {
