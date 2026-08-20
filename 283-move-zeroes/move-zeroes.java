b16ae48547f87b87a8bd73d771a477646a65c26e
Vijay Ausare <59534955+vijayausare@users.noreply.github.com>
2026-08-20 05:31:45 +0530

    Time: 1 ms (99.90%) | Memory: 48 MB (6.98%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -21,7 +21,27 @@ class Solution {
         */
     }
 
+    public void bitOptimized(int[] nums) {
+        int index = 0;
+        for(int num: nums) {
+            if(num!=0){
+                nums[index++] = num;
+            }
+        }
+
+        for(int idx = index; idx< nums.length; idx++) {
+            nums[idx] = 0;
+        }
+
+        /*
+        Analysis:
+        Time: O(n) + O(n) = O(2n) = O(n)
+        Space: O(1) ref only
+        */
+    }
+
     public void moveZeroes(int[] nums) {
-        this.bruteForceApproach(nums);
+        // this.bruteForceApproach(nums);
+        this.bitOptimized(nums);
     }
 }
