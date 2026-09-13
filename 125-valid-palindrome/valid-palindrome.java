fe1002c747adb92e9e54ae2719842b6f236d224a
Vijay Ausare <59534955+vijayausare@users.noreply.github.com>
2026-09-14 00:49:08 +0530

    Time: 16 ms (12.91%) | Memory: 47.5 MB (11.95%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,4 +1,36 @@
 class Solution {
+
+    public boolean reverseStringApproach(String s) {
+        String normalized = s.replaceAll("[^a-zA-Z0-9]", "") // ^ not, a-b, A-Z & 0-9
+                                .toLowerCase();
+        String reversed = new StringBuilder(normalized)
+                        .reverse()
+                        .toString();
+
+        return normalized.equals(reversed);
+
+        /*
+        1. TIME COMPLEXITY: O(n)
+
+        - Removing non-alphanumeric characters: O(n)
+        - Converting to lowercase: O(n)
+        - Creating and reversing StringBuilder: O(n)
+        - Creating the reversed String: O(n)
+        - Comparing the strings: O(n)
+
+        Final Time Complexity: O(n)
+
+
+        2. SPACE COMPLEXITY: O(n)
+
+        - The normalized String requires O(n) space.
+        - StringBuilder requires O(n) space.
+        - The reversed String requires O(n) space.
+
+        Final Auxiliary Space Complexity: O(n)
+        */
+
+    }
     public boolean towPointerApproach(String s) {
         int end = s.length() - 1;
         int start = 0;
@@ -51,6 +83,7 @@ class Solution {
     }
 
     public boolean isPalindrome(String s) {
-        return towPointerApproach(s);
+        return reverseStringApproach(s);
+        // return towPointerApproach(s);
     }
 }
