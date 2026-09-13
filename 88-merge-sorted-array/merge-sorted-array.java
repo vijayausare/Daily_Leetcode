22363b5e3a32eb3be760c839e6d575ad3d1549a8
Vijay Ausare <59534955+vijayausare@users.noreply.github.com>
2026-09-13 23:52:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (78.63%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -65,8 +65,40 @@ class Solution {
         */
     }
 
+    public void bitOptimizedTwoPointerApproach(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int idx = m + n - 1;
+
+        while(p1 >= 0 && p2 >= 0) {
+            if(nums1[p1] > nums2[p2]) {
+                nums1[idx] = nums1[p1];
+                p1--;
+            } else {
+                nums1[idx] = nums2[p2];
+                p2--;
+            }
+            idx--;
+        }
+
+        while(p2 >= 0) {
+            nums1[idx] = nums2[p2];
+            idx--;
+            p2--;
+        }
+
+      /*
+    1. TIME COMPLEXITY: O(m + n)
+    2. SPACE COMPLEXITY: O(1)
+       - No additional array or collection is created.
+       - Only three integer variables are used:
+         p1, p2 and idx.
+    */
+    }
+
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         // bruteForceApproach(nums1, m , nums2, n);
-        twoPointerApproach(nums1, m , nums2, n);
+        // twoPointerApproach(nums1, m , nums2, n);
+        bitOptimizedTwoPointerApproach(nums1, m , nums2, n);
     }
 }
