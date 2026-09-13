ac5440f2e061deeaa8f5b87cc1febec7a8fe2f00
Vijay Ausare <59534955+vijayausare@users.noreply.github.com>
2026-09-13 23:04:16 +0530

    Time: 3 ms (21.99%) | Memory: 44.1 MB (16.55%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -2,12 +2,24 @@ class Solution {
 
     // brute force approach
     public void bruteForceApproach(int[] nums1, int m, int[] nums2, int n) {
-        for(int i = 0; i< n; i++) {
-            nums1[m + i] = nums2[i]; 
-        }
-        Arrays.sort(nums1);
+    for(int i = 0; i < n; i++) {
+        nums1[m + i] = nums2[i]; 
     }
+    Arrays.sort(nums1);
 
+    /*
+    1. TIME COMPLEXITY: O(n) + O((m + n) * log(m + n))
+        - O(n): Time taken by the for-loop to copy nums2 elements.
+        - O((m + n) * log(m + n)): Time taken by DualPivotQuickSort to sort the full array.
+        
+        Final Time Complexity: O((m + n) * log(m + n)) 
+        (We drop the O(n) because the sorting time grows much faster and dominates).
+
+    2. SPACE COMPLEXITY: O(log(m + n))
+        - The algorithm modifies nums1 in-place (no new array created).
+        - However, DualPivotQuickSort uses O(log(m + n)) internal stack space for recursion.
+    */
+    }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         bruteForceApproach(nums1, m , nums2, n);
     }
